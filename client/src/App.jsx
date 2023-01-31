ea9618474dfcab2746bab729b7282b71028201d1
Prathamesh Jadhav <prathameshj0407@gmail.com>
2023-01-31 16:02:12 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
+import RegisterPage from "./Pages/RegisterPage";
+import GamePage from "./Pages/GamePage";
+
+// window.addEventListener(
+//   "contextmenu",
+//   function (e) {
+//     e.preventDefault();
+//   },
+//   false
+// );
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Register" element={<RegisterPage />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Play" element={<GamePage />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
