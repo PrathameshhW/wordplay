a38a27b3f7123fa439e414a075495f3f582c65a9
Prathamesh Jadhav <prathameshj0407@gmail.com>
2023-02-03 20:16:58 +0530

    first commit

--- client/src/Context/appContext.jsx
+++ client/src/Context/appContext.jsx
@@ -58,6 +58,7 @@ const AppProvider = ({ children }) => {
 
     try {
       const response = await axios.post("/api/v1/auth/register", currentUser);
+
       console.log(response);
 
       const { user, token } = response.data;
